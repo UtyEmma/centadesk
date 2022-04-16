92fb061810aea74d49d8788243a3f30dfba1994b
Utibe-Abasi Akpan <utyemma@gmail.com>
2022-04-16 16:30:43 +0100

    Changes

--- resources/views/components/page-banner.blade.php
+++ resources/views/components/page-banner.blade.php
@@ -4,7 +4,7 @@
         <img class="shape-1 animation-round" src="{{asset('images/shape/shape-8.png')}}" alt="Shape">
 
         <img class="shape-2" src="{{asset('images/shape/shape-23.png')}}" alt="Shape">
-        <div class="page-banner-content py-5 px-0 w-100">
+        <div class="page-banner-content py-5 px-0 w-100 mx-0">
 
             {{$slot}}
 
