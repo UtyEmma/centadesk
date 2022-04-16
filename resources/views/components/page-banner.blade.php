567de85da3046f1f0efeffb3a28f854085f733a0
Utibe-Abasi Akpan <utyemma@gmail.com>
2022-04-16 15:50:29 +0100

    Changes

--- resources/views/components/page-banner.blade.php
+++ resources/views/components/page-banner.blade.php
@@ -1,16 +1,15 @@
 <!-- Page Banner Start -->
-<div class="section page-banner py-0 pt-5 px-0 mx-0">
-    <img class="shape-1 animation-round" src="{{asset('images/shape/shape-8.png')}}" alt="Shape">
+<div class="section page-banner py-0 pt-5 px-0">
+    <div class="container">
+        <img class="shape-1 animation-round" src="{{asset('images/shape/shape-8.png')}}" alt="Shape">
 
-    <img class="shape-2" src="{{asset('images/shape/shape-23.png')}}" alt="Shape">
-    <div class="page-banner-content container pt-5 mt-5 mx-0">
-        <div class="p-0 mx-0 py-5 px-0 page-banner-content">
+        <img class="shape-2" src="{{asset('images/shape/shape-23.png')}}" alt="Shape">
+        <div class="page-banner-content py-5">
 
-        {{$slot}}
+            {{$slot}}
 
+            <img class="shape-3" src="{{asset('images/shape/shape-24.png')}}" alt="Shape">
         </div>
-
-        <img class="shape-3" src="{{asset('images/shape/shape-24.png')}}" alt="Shape">
     </div>
 </div>
 <!-- Page Banner End -->
