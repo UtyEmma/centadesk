ac53510ce2abface3b29b835a75fe947daca32ae
Utibe-Abasi Akpan <utyemma@gmail.com>
2022-04-16 16:24:51 +0100

    Changes

--- resources/views/components/page-banner.blade.php
+++ resources/views/components/page-banner.blade.php
@@ -1,10 +1,10 @@
 <!-- Page Banner Start -->
 <div class="section page-banner py-0 pt-5 px-0 w-100">
-    <div class="container ">
+    <div class="container">
         <img class="shape-1 animation-round" src="{{asset('images/shape/shape-8.png')}}" alt="Shape">
 
         <img class="shape-2" src="{{asset('images/shape/shape-23.png')}}" alt="Shape">
-        <div class="page-banner-content py-5 px-0 w-100 d-flex justify-content-center">
+        <div class="page-banner-content py-5 px-0 w-100">
 
             {{$slot}}
 
